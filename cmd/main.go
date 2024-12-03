6329c77debeb32ae7166512797eba9909c01f38d
John Nganga <mukunajohn329@gmail.com>
2024-12-03 18:59:28 +0300

    hanlder

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"net/http"
+	"time"
+
+	"girhub.com/flexGURU/go-gin/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func main() {
+
+	router := gin.Default()
+
+	// Router
+	router.GET("/getData", handlers.GetData)
+	router.POST("/postData", handlers.PostData)
+	router.POST("/getQS", handlers.GetQueryString)
+	router.GET("/getQueryParams/:name/:age", handlers.GetQueryParams)
+
+
+	server := http.Server{
+		Addr: ":8080",
+		Handler: router,
+		ReadTimeout: 10*time.Second,
+		WriteTimeout: 10*time.Second,
+	}
+
+	server.ListenAndServe()
+	
+
+
+
+}
---
+++ handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Handler function to get serve the GET request /getData
+func GetData(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"data": "here is the data",
+	})
+}
+
+
+// 
+func PostData(ctx *gin.Context) {
+
+	body := ctx.Request.Body
+	value, _ := io.ReadAll(body)
+
+	ctx.JSON(200, gin.H{
+		"data": fmt.Sprintf("%s", value),
+	})
+}
+
+// Query strings
+// eg URL: http://localhost:8080/getQS?name=John&age=30
+func GetQueryString(ctx *gin.Context) {
+
+	name := ctx.Query("name")
+	age := ctx.Query("age")
+
+	ctx.JSON(200, gin.H{
+		"data": fmt.Sprintf("values are name: %s and age: %s", name, age),
+	})
+}
+// Query parameters
+// eg URL: http://localhost:8080/getQueryParams/John/30
+func GetQueryParams(ctx *gin.Context) {
+
+	name := ctx.Param("name")
+	age := ctx.Param("age")
+
+	ctx.JSON(200, gin.H{
+		"data": fmt.Sprintf("values are name: %s and age: %s", name, age),
+	})
+
+
+
+}
